d0df3365d89b39b0f73bcc734e8afadd3a8a8ad8
vorontsov <vorontsov677@gmail.com>
2023-03-03 11:19:34 +0100

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by ANDREY VORONTSOV on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
